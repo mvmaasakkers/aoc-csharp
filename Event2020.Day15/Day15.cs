c90c06bf63d88dde475758bb893da4360a023c15
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2020-12-15 11:05:45 +0100

    Added Day15 of AoC 2020

---
+++ Event2020.Day15/Day15.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Event2020.Day15
+{
+    public class Day15
+    {
+        private List<int> _input;
+
+        public Day15(string input)
+        {
+            _input = input.Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(t => int.Parse(t.Trim())).ToList();
+        }
+
+        public long ComputePart1()
+        {
+            return Play(2020);
+        }
+
+        int Play(int rounds)
+        {
+            var mem = new Dictionary<int, List<int>> { };
+            var lastNumber = 0;
+            for (int i = 0; i < rounds; i++)
+            {
+                if (_input.Count - 1 >= i && !mem.ContainsKey(_input[i]))
+                {
+                    lastNumber = _input[i];
+                }
+                else if (!mem.ContainsKey(lastNumber))
+                {
+                    lastNumber = 0;
+                }
+                else if (mem.ContainsKey(lastNumber) && mem[lastNumber].Count > 1 &&
+                         mem[lastNumber][mem[lastNumber].Count - 2] < i - 1)
+                {
+                    lastNumber = i - 1 - mem[lastNumber][mem[lastNumber].Count - 2];
+                }
+                else if (mem.ContainsKey(lastNumber) && mem[lastNumber].Count > 0 &&
+                         mem[lastNumber][mem[lastNumber].Count - 1] == i - 1)
+                {
+                    lastNumber = 0;
+                }
+
+                if (!mem.ContainsKey(lastNumber))
+                {
+                    mem[lastNumber] = new List<int>();
+                }
+
+                mem[lastNumber].Add(i);
+            }
+
+            return lastNumber;
+        }
+
+        public long ComputePart2()
+        {
+            return Play(30000000);
+        }
+    }
+}
