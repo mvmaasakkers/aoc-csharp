e6128936bbbd770acd50ea0a5a96c35d18414dd9
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2020-12-16 15:24:29 +0100

    Added empty day16

---
+++ Event2020.Day16/Day16.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Event2020.Day16
+{
+    public class Day16
+    {
+        private List<string> _input;
+
+        public Day16(string input)
+        {
+            _input = input.Split(new[] {"\r\n", "\n"}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(t => t.Trim()).ToList();
+        }
+
+        public long ComputePart1()
+        {
+            return 0;
+        }
+
+        public long ComputePart2()
+        {
+            return 0;
+        }
+    }
+}
